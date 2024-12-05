d8e2515556bac11c688d3059a65d9d47f2408e6c
matchaism <60777705+matchaism@users.noreply.github.com>
2024-12-05 13:34:40 +0900

    fix slack.js

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ const config = {
   // グローバル定数
   DAYS_IN_CALENDAR: 25, // カレンダーの日数
   YEAR: PropertiesService.getScriptProperties().getProperty('sheet_name'), // 対象の年
-  WEBHOOK_URL: PropertiesService.getScriptProperties().getProperty('webhook_url'), // WebhookのURL
+  WEBHOOK_URL: PropertiesService.getScriptProperties().getProperty('slack_webhook_url'), // WebhookのURL
 }

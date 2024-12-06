6a7a14b818aab34b40ad6313bbe6316a1a183aba
matchaism <60777705+matchaism@users.noreply.github.com>
2024-12-06 17:55:22 +0900

    add js for discord

---
+++ src/discord.js
@@ -0,0 +1,32 @@
+const discord = {
+  generatePostedArticlePayloads: function(calendarEntry) {
+    const payloads = [];
+
+    for (let i = 0; i < config.DAYS_IN_CALENDAR; i++) {
+      // 新しい投稿があるか
+      if (calendarEntry.calendarStatus[i] === 'posted') {
+        // 投稿メッセージを作成
+        const message = `${calendarEntry.authors[i]} posted <${calendarEntry.articles[i]}|Day ${i + 1} Article>!!`
+        // ペイロードを作成
+        payloads.push({
+          'username': calendarEntry.custom.botName,
+          //'icon_emoji': calendarEntry.custom.iconEmoji, -> avatar_url?
+          'content': message
+        });
+      }
+    }
+
+    return payloads;
+  },
+
+  // WebhookにPOSTリクエストを送信
+  postToWebhook: function(url, payload) {
+    const options = {
+      'method': 'post',
+      'contentType': 'application/json',
+      'payload': JSON.stringify(payload)
+    };
+    // post
+    UrlFetchApp.fetch(url, options);
+  },
+}
--- src/main.js
+++ src/main.js
@@ -36,5 +36,19 @@ function main() {
       }
     }
 
+    // Discord
+    if (config.Discord_WEBHOOK_URL !== null) {
+      // 投稿があれば通知用のペイロードを生成
+      const payloads = discord.generatePostedArticlePayloads(calendarDifference);
+      Logger.log(payloads);
+      // 投稿があれば通知
+      for (let i = 0; i < payloads.length; i++) {
+        if (payloads[i] !== null) {
+          discord.postToWebhook(config.Discord_WEBHOOK_URL, payloads[i]);
+          Utilities.sleep(500); // 0.5秒待機 (Slackのレート制限:2 per second)
+        }
+      }
+    }
+
   }
 }
